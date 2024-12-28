405fcc2a77b5d7188bc03b9e28807a56ced75405
ankit-prabhavak <ankitabcd1718@gmail.com>
2024-12-28 18:27:23 +0530

    updates main.py

--- main.py
+++ main.py
@@ -4,20 +4,19 @@ import random
 import pyttsx3
 
 engine = pyttsx3.init()
-engine.say("Lets start the game. Please guess the number and enter.")
-engine.runAndWait()
-
-n=random.randint(1,10)
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+speak("Lets start the game. Please guess the number and enter.")
+
+n=random.randint(1,10)
 
 a=-1
 Guesses=0
 while a!=n:
-    a = int(input("Guess the number:"))
+    a = int(input("Guess the number: "))
     Guesses+=1
     if(a>n):
         print("Too high")
